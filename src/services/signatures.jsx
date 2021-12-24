f0af36ec7029c2f0413ed0db9f703688d705b5c9
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2021-12-24 13:31:45 -0800

    services, DB functions drafted

---
+++ src/services/signatures.jsx
@@ -0,0 +1,38 @@
+import { client, parseData } from './client';
+
+export async function getSignatures() {
+  const request = await client.from('signatures').select().order('last_name');
+  return parseData(request);
+}
+
+export async function getSignatureById(id) {
+  const request = await client
+    .from('signatures')
+    .select('*')
+    .match({ id })
+    .single();
+  return parseData(request);
+}
+
+export async function updateSignatureById(
+  id,
+  { first_name, last_name, email }
+) {
+  const request = await client
+    .from('signatures')
+    .update({ first_name, last_name, email })
+    .match({ id });
+  return parseData(request);
+}
+
+export async function createSignature({ first_name, last_name, email }) {
+  const request = await client
+    .from('signatures')
+    .insert({ first_name, last_name, email });
+  return parseData(request);
+}
+
+export async function deleteSignatureById(id) {
+  const request = await client.from('signatures').delete().match({ id });
+  return parseData(request);
+}
