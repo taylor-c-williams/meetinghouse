df8822ac588b7a49b88531ccb555683cc9a49218
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2021-12-24 19:09:55 -0800

    signatureList functional

--- src/components/Mission.jsx
+++ src/components/Mission.jsx
@@ -14,39 +14,6 @@ export default function Mission() {
       iaculis. Morbi lobortis porta magna, at tempus enim. Fusce quis neque
       erat. Duis volutpat pulvinar fermentum. In mattis eget diam vitae
       lobortis. Donec egestas auctor orci, et bibendum magna efficitur vitae.
-      Nullam ut tincidunt nisi. Praesent vestibulum mauris et est lobortis
-      elementum. Nam interdum ut lorem id dictum. Nullam scelerisque bibendum
-      nisi eget porta. Aliquam commodo erat lacus, sit amet dictum ipsum
-      efficitur a. Praesent pulvinar ac turpis ac dictum. Morbi eu nunc
-      vulputate, vulputate urna pellentesque, scelerisque libero. Vestibulum
-      pretium odio pulvinar finibus auctor. Ut non orci in orci placerat
-      venenatis vitae et neque. Ut elementum diam nulla, sit amet fringilla
-      mauris sodales vel. Curabitur rhoncus consectetur hendrerit. Vestibulum
-      aliquam rhoncus tincidunt. Curabitur in euismod lectus. Suspendisse id
-      condimentum nisl, consectetur viverra nisl. Vivamus sed dolor euismod,
-      consequat dolor sit amet, ultrices elit. Fusce eget rhoncus sapien, a
-      dictum diam. Aenean ut lobortis metus. Pellentesque condimentum sodales
-      erat imperdiet condimentum. Integer nunc mi, aliquam at massa vel,
-      ullamcorper vulputate nisi. Quisque nec risus quis sem dapibus tempus.
-      Donec pretium commodo magna finibus egestas. Proin lobortis tincidunt
-      purus, eget pretium metus dapibus ut. Suspendisse arcu libero, facilisis
-      ut quam vel, tristique interdum nisi. Pellentesque nec sapien id augue
-      sodales elementum. Duis vitae erat aliquam, viverra tellus nec, viverra
-      lacus. Nullam mattis maximus ex, sit amet cursus justo consequat lobortis.
-      Praesent sagittis elit vel nibh accumsan, sed convallis eros vulputate.
-      Nulla et rutrum orci. In fermentum ipsum non finibus consequat. Maecenas
-      feugiat fermentum enim, sed pretium neque congue a. Curabitur arcu mi,
-      varius lobortis aliquam id, consectetur a urna. Aliquam metus est,
-      consectetur in mi sed, egestas mollis magna. Cras gravida, enim eget
-      egestas fermentum, enim velit luctus elit, quis dapibus lacus velit et
-      urna. Pellentesque posuere eu est quis interdum. Vestibulum gravida enim
-      sit amet odio dapibus vulputate. Maecenas eget ex vel odio bibendum tempus
-      et ut elit. Pellentesque ac libero faucibus, feugiat lectus eu, lobortis
-      magna. Morbi vel congue nisl, vitae ornare sapien. Sed justo est, tempor
-      pellentesque mauris nec, blandit viverra ante. Sed aliquam urna nec augue
-      rutrum, maximus fermentum orci ultrices. Aliquam erat volutpat. Nullam in
-      condimentum augue. Generated 5 paragraphs, 406 words, 2780 bytes of Lorem
-      Ipsum
     </section>
   );
 }
--- src/components/SignatureList.jsx
+++ src/components/SignatureList.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { getSignatures } from '../services/signatures';
 
 export default function SignatureList() {
+  const [signatures, setSignatures] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getSignatures()
+      .then((res) => setSignatures(res))
+      .finally(() => setLoading(false));
+  }, []);
+
   return (
     <section className="list">
-      list of sigs
+      <h1>Neighbors in Support:</h1>
       <ul>
-        <li>one</li>
-        <li>two</li>
-        <li>three</li>
+        {signatures.map((signature) => {
+          return (
+            <li key={signature.id}>
+              {signature.first_name} {signature.last_name}
+            </li>
+          );
+        })}
       </ul>
     </section>
   );
