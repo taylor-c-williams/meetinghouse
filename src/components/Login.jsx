df916fc040c5048fea00349a78bfbc06fc81bb57
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2021-12-25 19:24:23 -0800

    Login component drafted

---
+++ src/components/Login.jsx
@@ -0,0 +1,27 @@
+import { useRef, useState } from 'react';
+
+export default function Login() {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    //  TODO add login logic
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="input-email">Email</label>
+        <input id="input-email" type="email" ref={emailRef} />
+
+        <label htmlFor="input-password">Password</label>
+        <input id="input-password" type="password" ref={passwordRef} />
+
+        <br />
+
+        <button type="submit">Login</button>
+      </form>
+    </>
+  );
+}
